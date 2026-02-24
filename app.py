5970fccdce5f4e77bdfc540b6d2b558923ff8eb6
John Ramil Mapatac <mapatacjohnramil.icct.edu.ph@gmail.com>
2026-02-24 18:11:41 +0000

    Debug app.py file

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def home():
     return render_template('index.html', results=results, inputs=inputs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
